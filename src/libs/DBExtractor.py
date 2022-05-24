cc3b0bf35044b4b014d5f83427ded87691043e67
Manuel Duro <manueldurosantos@gmail.com>
2022-05-24 11:34:50 +0200

    Refactor con consulta SQL más elaborada

--- src/libs/DBExtractor.py
+++ src/libs/DBExtractor.py
@@ -2,64 +2,50 @@ import json
 import pyodbc
 import pandas as pd
 
+
 class DBExtractor():
     def __init__(self, configFile: str):
         f = open(configFile)
         data = json.load(f)
-        
+
         self._HOST = data["HOST"]
         self._PORT = data["PORT"]
         self._DATABASE = data["DATABASE"]
         self._USER = data["USER"]
         self._PASSWORD = data["PASSWORD"]
-        
+
         f.close()
- 
 
     def extract(self, targetFile: str):
         conn = None
         try:
             conn = pyodbc.connect("DRIVER={ODBC Driver 18 for SQL Server}" +
-                                ";SERVER=" + self._HOST + 
-                                ";DATABASE=" + self._DATABASE + 
-                                ";UID=" + self._USER + 
-                                ";PWD=" + self._PASSWORD + 
-                                ";TrustServerCertificate=Yes")
-            
-            # Insert your exercise code here
-            df = pd.read_sql("SELECT ItemId, ItemDocumentNbr, CustomerName, CreateDate, UpdateDate,"
-                             "VersionNbr, DeletedFlag FROM Item JOIN Customer "
-                             "ON Item.CustomerId = Customer.CustomerId;", conn)
+                                  ";SERVER=" + self._HOST +
+                                  ";DATABASE=" + self._DATABASE +
+                                  ";UID=" + self._USER +
+                                  ";PWD=" + self._PASSWORD +
+                                  ";TrustServerCertificate=Yes")
 
-            df = self.clean_older_versions(df)
-            df = self.clean_inactive_items(df)
+            # Insert your exercise code here
+            df = pd.read_sql("SELECT c.ItemId, c.ItemDocumentNbr, c.CustomerName, c.CreateDate, c.UpdateDate "
+                             "FROM (SELECT Item.*, CustomerName, "
+                             "ROW_NUMBER() OVER(PARTITION BY ItemId ORDER BY ItemId,VersionNbr DESC) AS rnum "
+                             "FROM Item JOIN Customer ON Item.CustomerId = Customer.CustomerId ) as c "
+                             "WHERE rnum<=1 AND c.DeletedFlag = 0;", conn)
+
+            ''''''
 
             self.add_item_source_column(df)
 
-            df = self.clean_unused_columns(df)
-
-            df.to_csv(targetFile,';','',line_terminator='\r\n', quotechar='"',
-                      index=False,encoding='UTF-8', compression='gzip')
+            df.to_csv(targetFile, ';', '', line_terminator='\r\n', quotechar='"',
+                      index=False, encoding='UTF-8', compression='gzip')
 
             # End of exercise
         except Exception as e:
-            print("error extracting data from sqlserver"+e)
-        finally:        
+            print("error extracting data from sqlserver" + e)
+        finally:
             if conn: conn.close()
-
-    def clean_unused_columns(self, df):
-        df = df.drop(columns=['VersionNbr', 'DeletedFlag'])
-        return df
 
     def add_item_source_column(self, df):
         df['ItemSource'] = ['Local' if (x[-2:] == '66' or x[-2:] == '99')
                             else 'External' for x in df['CustomerName']]
-
-    def clean_inactive_items(self, df):
-        df = df[df.DeletedFlag != 1]
-        return df
-
-    def clean_older_versions(self, df):
-        df = df.sort_values(by=['ItemId', 'VersionNbr'])
-        df = df.drop_duplicates(subset="ItemId", keep='last')
-        return df
